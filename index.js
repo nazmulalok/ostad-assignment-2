dbaf5b21477bfdb40a6fa4f91c98aeb581040238
nazmulalok <you@example.com>
2024-06-27 22:30:21 +0600

    Question 2 solved

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function calculateSum(a, b) {
 }
 const result = calculateSum(12, 8);
 console.log(result);
+
+// Question - 2
+function isEven(num) {
+    return num % 2 === 0;
+}
+const result2 = isEven(11);
+console.log(result2);
