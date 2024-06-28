f190c2cdfe6e4a2bb9ecb39a071bbeb1899514f8
nazmulalok <you@example.com>
2024-06-28 15:12:10 +0600

    Question 3 solved

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function isEven(num) {
 }
 const result2 = isEven(11);
 console.log(result2);
+
+// Question - 3
+function findMax(arr) {
+    if (arr.length === 0) {
+        throw new Error("Array is empty");
+    }
+    let maxnum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxnum) {
+            maxnum = arr[i];
+        }
+    }
+    return maxnum;
+}
+
+const numbers = [3, 7, 2, 9, 5];
+const result3 = findMax(numbers);
+console.log(result3);
